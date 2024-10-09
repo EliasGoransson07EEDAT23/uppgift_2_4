d37a65882281feec282d98ba40ff43b2bdf286f3
elgo07001 <elgo07001@E-PW05C37M>
2024-10-09 14:44:11 +0200

    Add project files.

---
+++ uppgift_2_4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Uppgift_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lönekollen; Se medellönen på dina anställda!");
+            Console.WriteLine("Dina anställda är Rebecca, Alfred, Morgan.");
+            Console.WriteLine("Skriv Rebeccas lön nedan;");
+            int Rebecca = int.Parse(Console.ReadLine());
+            Console.WriteLine("Skriv Alfreds lön nedan;");
+            int Alfred = int.Parse(Console.ReadLine());
+            Console.WriteLine("Skriv Morgans lön nedan;");
+            int Morgan = int.Parse(Console.ReadLine());
+            int msum = Alfred + Rebecca + Morgan;
+            int medel = msum / 3;
+
+            Console.WriteLine("Medellönen på dina anställda är " + medel + "kr.");
+            Console.ReadKey();
+        }
+    }
+}
